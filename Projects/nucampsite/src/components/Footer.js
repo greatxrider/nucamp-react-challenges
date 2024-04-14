13f9abfa5b0a21ac621ae362b1ceeb01746fae37
Jeph Mari Daligdig <daligdigjeph09@gmail.com>
2024-04-14 23:47:55 +0800

    update

--- Projects/nucampsite/src/components/Footer.js
+++ Projects/nucampsite/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col xs={{ size: 4, offset: 1 }} sm='2'>
-                        <h5>this will be site navigation!</h5>
+                        <h5>Site Navigation!</h5>
                     </Col>
                     <Col xs='6' sm='3' className='text-center'>
                         <h5>Social</h5>

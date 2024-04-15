b6a1b510077ab993398af11218224a0983d23d07
Jeph Mari Daligdig <daligdigjeph09@gmail.com>
2024-04-15 22:51:59 +0800

    numcapsite project update

--- Projects/nucampsite/src/App.js
+++ Projects/nucampsite/src/App.js
@@ -1,18 +1,28 @@
 // import React from 'react'
 // import logo from './logo.svg';
 // import { Counter } from './features/counter/Counter';
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import './App.css'
 // import { Container, Navbar, NavbarBrand } from 'reactstrap'
 // import NucampLogo from './app/assets/img/logo.png'
-import CampsitesList from './features/campsites/CampsitesList';
+// import CampsitesList from './features/campsites/CampsitesList';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 
 function App() {
   return (
     <div>
       <Header />
-      <CampsitesList />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+      </Routes>
       <Footer />
     </div>
   )

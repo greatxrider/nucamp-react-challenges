471d930fd08109633bdfe5bef74a2f6c19eb1fd4
Jeph Mari Daligdig <daligdigjeph09@gmail.com>
2024-04-20 04:42:59 +0800

    update

--- Projects/nucampsite/src/features/display/DisplayCard.js
+++ Projects/nucampsite/src/features/display/DisplayCard.js
@@ -2,6 +2,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 const DisplayCard = ({ item }) => {
     const { image, name, description } = item;
+
     return (
         <Card>
             <CardImg src={image} alt={name} />
